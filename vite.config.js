8101b5dccd6862b0883a9a9989e8414f2c35c4d0
Kelly E Cronin Krein <keckrein@gmail.com>
2026-09-02 10:31:21 -0500

    Replace WorldCupApp with WorldCupPredictor

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import WorldCupApp from "./worldcup2026.jsx";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <WorldCupApp />
+  <WorldCupPredictor />
 );
